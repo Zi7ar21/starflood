328f6a745e7c0bcba6d503ff2beb34583771b650
Jacob Bingham <41303821+Zi7ar21@users.noreply.github.com>
2023-11-21 22:25:09 -0700

    Barnes-Hut Force Calculation
    Now a complete N-body code!

--- README.md
+++ README.md
@@ -4,28 +4,32 @@ Starflood is an open-source SPH and N-body code(s) written in C/C++.
 
 ## Features
 
-- Semi-simultaneous simulation and rendering
+- [Smoothed-particle hydrodynamics (SPH)](https://en.wikipedia.org/wiki/Smoothed-particle_hydrodynamics)
+- _O(N*log(N))_-complexity tree gravity solver using the [Barnes-Hut algorithm](https://en.wikipedia.org/wiki/Barnes%E2%80%93Hut_simulation) O(NlogN)
 - Decently optimized low-level C/C++ code with high-level parallelism via [OpenMP](www.openmp.org)
+- Semi-simultaneous simulation and rendering
 - Performance profiling using `omp_get_wtime` for `double`-precision timing (a little less precision than a nanosecond after 6 months uptime)
-- Particle-Particle Gravity Solver O(N²)
 
 ### Planned
 
+- Combine all sources into one clean, configurable, 2D/3D SPH/N-body code
+- Optimize SPH neighbor search to be better than _O(N²)_
+- Multipole Barnes-Hut or Fast-Multipole Method
 - Ability to save/load simulation data and render it afterwards
 - Ability to load pre-generated initial conditions
-- Ability to toggle between 2D/3D simulation/rendering
-- Device offloading (development currently made difficult due to [a lack of compiler packages with build support on Arch Linux](https://bugs.archlinux.org/task/63227))
-- Tree Gravity Solver using the [Barnes-Hut algorithm](https://en.wikipedia.org/wiki/Barnes%E2%80%93Hut_simulation) O(NlogN)
-- [Smoothed-particle hydrodynamics (SPH)](https://en.wikipedia.org/wiki/Smoothed-particle_hydrodynamics)
+- GPU/device offloading (development is currently made difficult due to [a lack of compiler packages with build support on Arch Linux](https://bugs.archlinux.org/task/63227), and NVIDIA's HPC SDK requires AVX so that's out of the question...)
 - Volumetrically rendering SPH particles for eye candy visualization
 - A more standardized build system such as [CMake](https://cmake.org/)
 
 ## Credits
 
-- Philip Mocz's Python SPH Guide https://github.com/pmocz/sph-python
+- Philip Mocz's Python SPH Guide
+  -  <https://github.com/pmocz/sph-python>
   - Used as a reference for the SPH algorithm, although the numpy syntax was a little hard to decipher...
 - stb https://github.com/nothings/stb
-  - `stb_image.h` and `stb_image_write.h`
+  - `stb_image.h` and `stb_image_write.h` headers for image read/write
+- Jeffrey Heer's article _The Barnes-Hut Approximation: Efficient computation of N-body forces_
+  - <https://jheer.github.io/barnes-hut/>
 
 ## Installation
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,12 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
+#define FRAME_INTERVAL 1
+#define FRAMES 300
+#define TIMESTEP (4.0*0.0314159265358979323846264338327950288419716939937510582097494459)
+
+#define STARFLOOD_RENDER_INTERACTS
+
 #define STARFLOOD_ENABLE_PROFILING
 
 typedef float real;
@@ -78,30 +84,6 @@ void drawLine(float* image, int w, int h, float r, float g, float b, float a, in
 	}
 }
 
-class Node {
-	public:
-		int parent;
-		int child0;
-		int child1;
-		int child2;
-		int child3;
-
-		int id;
-		int np;
-		float m;
-		float x;
-		float y;
-
-		Node(): parent(-1), child0(-1), child1(-1), child2(-1), child3(-1), id(-1), np(0), m(0.0f), x(0.0f), y(0.0f) {}
-
-		Node(int _parent): parent(_parent), child0(-1), child1(-1), child2(-1), child3(-1), id(-1), np(0), m(0.0f), x(0.0f), y(0.0f) {}
-};
-
-/*
-void Remove() {
-}
-*/
-
 void drawLineUV(float* image, int w, int h, float r, float g, float b, float a, real x0, real y0, real x1, real y1) {
 	drawLine(image, w, h, r, g, b, a,
 	(int)((real)h * x0 + (real)0.5 * (real)w),
@@ -110,12 +92,62 @@ void drawLineUV(float* image, int w, int h, float r, float g, float b, float a, 
 	(int)((real)h * y1 + (real)0.5 * (real)h));
 }
 
-void BarnesHut(std::vector<Node> tree, float* image, int w, int h, int* ids, real* pos, real* acc, real bx_min, real by_min, real bx_max, real by_max, int N) {
+class Node {
+	public:
+		/*
+		Quadrants:
+		+---+---+
+		| 2 | 3 |
+		+---+---+
+		| 0 | 1 |
+		+---+---+
+		*/
+
+		int parent; // index of this node's parent, -1: this is the root node
+		int child0; // index of this node's child in quadrant 0
+		int child1; // index of this node's child in quadrant 1
+		int child2; // index of this node's child in quadrant 2
+		int child3; // index of this node's child in quadrant 3
+
+		int id; // id of the particle contained
+		int np; // number of particles contained in this node and children
+
+		real x_min; // this node's axis-aligned bounding-box x-minimum
+		real y_min; // this node's axis-aligned bounding-box y-minimum
+		real x_max; // this node's axis-aligned bounding-box x-maximum
+		real y_max; // this node's axis-aligned bounding-box y-maximum
+
+		real m; // mass contained in this node and children
+		real x; // this node's center of mass in the x-axis
+		real y; // this node's center of mass in the y-axis
+
+		Node() {}
+
+		Node(int _parent, real _x_min, real _y_min, real _x_max, real _y_max):
+		parent(_parent), child0(-1), child1(-1), child2(-1), child3(-1), id(-1), np(0),
+		x_min(_x_min), y_min(_y_min), x_max(_x_max), y_max(_y_max), m(0), x(0), y(0) {}
+};
+
+void BarnesHut(std::vector<Node> tree, float* image, int w, int h, int* ids, real* mas, real* pos, real* acc, int N) {
 	for(int i = 0; i < N; i++) ids[i] = -1;
 
 	tree.clear();
 
-	tree.push_back(-1);
+	//tree.push_back(Node(-1,(real)-1.0,(real)-1.0,(real)1.0,(real)1.0));
+
+	tree.push_back(Node(-1,pos[0],pos[1],pos[0],pos[1]));
+
+	for(int i = 1; i < N; i++) {
+		tree[0].x_min = fmin(tree[0].x_min,pos[2*i+0]);
+		tree[0].y_min = fmin(tree[0].y_min,pos[2*i+1]);
+		tree[0].x_max = fmax(tree[0].x_max,pos[2*i+0]);
+		tree[0].y_max = fmax(tree[0].y_max,pos[2*i+1]);
+	}
+
+	tree[0].x_min -= 0.1;
+	tree[0].y_min -= 0.1;
+	tree[0].x_max += 0.1;
+	tree[0].y_max += 0.1;
 
 	/*
 	Quadrants:
@@ -130,16 +162,16 @@ void BarnesHut(std::vector<Node> tree, float* image, int w, int h, int* ids, rea
 	for(int i = 0; i < N; i++) {
 		int current_node = 0;
 
-		real min_x = bx_min;
-		real min_y = by_min;
-		real max_x = bx_max;
-		real max_y = by_max;
-
-		int depth = 1;
+		int depth = 0;
 
 		while(depth <= 12) {
-			real hx = ((real)0.5*min_x)+((real)0.5*max_x);
-			real hy = ((real)0.5*min_y)+((real)0.5*max_y);
+			real x_min = tree[current_node].x_min;
+			real y_min = tree[current_node].y_min;
+			real x_max = tree[current_node].x_max;
+			real y_max = tree[current_node].y_max;
+
+			real hx = ((real)0.5*tree[current_node].x_min)+((real)0.5*tree[current_node].x_max);
+			real hy = ((real)0.5*tree[current_node].y_min)+((real)0.5*tree[current_node].y_max);
 
 			if(tree[current_node].child0 != -1
 			|| tree[current_node].child1 != -1
@@ -150,23 +182,15 @@ void BarnesHut(std::vector<Node> tree, float* image, int w, int h, int* ids, rea
 
 				switch(q) {
 					case 0:
-					max_x = hx;
-					max_y = hy;
 					current_node = tree[current_node].child0;
 					break;
 					case 1:
-					min_x = hx;
-					max_y = hy;
 					current_node = tree[current_node].child1;
 					break;
 					case 2:
-					max_x = hx;
-					min_y = hy;
 					current_node = tree[current_node].child2;
 					break;
 					case 3:
-					min_x = hx;
-					min_y = hy;
 					current_node = tree[current_node].child3;
 					break;
 					default:
@@ -177,31 +201,30 @@ void BarnesHut(std::vector<Node> tree, float* image, int w, int h, int* ids, rea
 				depth++;
 
 				continue;
-			} 
-
-			//if(depth < 6) {
-			if(true) {
-				drawLineUV(image, w, h, 0., .5, 0., 1., min_x, min_y, max_x, min_y);
-				drawLineUV(image, w, h, 0., .5, 0., 1., min_x, min_y, min_x, max_y);
-				drawLineUV(image, w, h, 0., .5, 0., 1., min_x, max_y, max_x, max_y);
-				drawLineUV(image, w, h, 0., .5, 0., 1., max_x, min_y, max_x, max_y);
-				drawLineUV(image, w, h, 0., .5, 0., 1., min_x,    hy, max_x,    hy);
-				drawLineUV(image, w, h, 0., .5, 0., 1.,    hx, min_y,    hx, max_y);
 			}
 
 			// node is not split
 			if(tree[current_node].id != -1) {
+				/*
+				Quadrants:
+				+---+---+
+				| 2 | 3 |
+				+---+---+
+				| 0 | 1 |
+				+---+---+
+				*/
+
 				// node has particle
-				tree.push_back(current_node);
+				tree.push_back(Node(current_node,x_min,y_min,   hx,   hy));
 				tree[current_node].child0 = tree.size() - 1;
 
-				tree.push_back(current_node);
+				tree.push_back(Node(current_node,   hx,y_min,x_max,   hy));
 				tree[current_node].child1 = tree.size() - 1;
 
-				tree.push_back(current_node);
+				tree.push_back(Node(current_node,x_min,   hy,   hx,y_max));
 				tree[current_node].child2 = tree.size() - 1;
 
-				tree.push_back(current_node);
+				tree.push_back(Node(current_node,   hx,   hy,x_max,y_max));
 				tree[current_node].child3 = tree.size() - 1;
 
 				int id = tree[current_node].id;
@@ -238,6 +261,15 @@ void BarnesHut(std::vector<Node> tree, float* image, int w, int h, int* ids, rea
 			// node doesn't have particle
 			tree[current_node].np = 1;
 			tree[current_node].id = i;
+
+			int tmpid = current_node;
+			while(tmpid != -1) {
+				tree[tmpid].m += mas[i];
+				tree[tmpid].x += mas[i]*pos[2*i+0];
+				tree[tmpid].y += mas[i]*pos[2*i+1];
+				tmpid = tree[tmpid].parent;
+			}
+
 			break;
 		}
 
@@ -258,24 +290,77 @@ void BarnesHut(std::vector<Node> tree, float* image, int w, int h, int* ids, rea
 
 	// Compute Forces
 	for(int i = 0; i < N; i++) {
+		acc[2*i+0] = (real)0;
+		acc[2*i+1] = (real)0;
+
+		real u_i = pos[2*i+0];
+		real v_i = pos[2*i+1];
+
 		std::stack<int> to_visit;
 
 		to_visit.push(0);
 
 		int depth = 0;
 
-		real x_min = bx_min;
-		real y_min = by_min;//width/dist;
-		real x_max = bx_max;
-		real y_max = by_max;
-
 		while(!to_visit.empty()) {
-			real hx = ((real)0.5*x_min)+((real)0.5*x_max);
-			real hy = ((real)0.5*x_min)+((real)0.5*x_max);
+			//real hx = ((real)0.5*x_min)+((real)0.5*x_max);
+			//real hy = ((real)0.5*x_min)+((real)0.5*x_max);
 
 			int cur_node = to_visit.top();
 
 			to_visit.pop();
+
+			if((tree[cur_node].np < 1) || (tree[cur_node].id == i)) continue;
+
+			real x_min = tree[cur_node].x_min, y_min = tree[cur_node].y_min, x_max = tree[cur_node].x_max, y_max = tree[cur_node].y_max;
+
+			real width = ((real)0.5*(x_max - x_min))+((real)0.5*(y_max - y_min));
+
+			real dx = tree[cur_node].x;
+			real dy = tree[cur_node].y;
+			real dm = tree[cur_node].m;
+
+			if(dm < (real)0.001) continue;
+
+			if(x_min < u_i && u_i < x_max && y_min < v_i && v_i < y_max) {
+				dx *= dm;
+				dy *= dm;
+				dm -= mas[i];
+				dx -= mas[i]*pos[2*i+0];
+				dy -= mas[i]*pos[2*i+1];
+				dx /= dm;
+				dy /= dm;
+			}
+
+			dx = dx-u_i;
+			dy = dy-v_i;
+
+			real dist = sqrtf((dx*dx)+(dy*dy));
+
+			if(dm < (real)0.001) continue;
+
+			const real theta = 1.0;
+
+			if((width/dist) > theta) {
+				if(tree[cur_node].child0 != -1) to_visit.push(tree[cur_node].child0);
+				if(tree[cur_node].child1 != -1) to_visit.push(tree[cur_node].child1);
+				if(tree[cur_node].child2 != -1) to_visit.push(tree[cur_node].child2);
+				if(tree[cur_node].child3 != -1) to_visit.push(tree[cur_node].child3);
+				continue;
+			}
+
+			#ifdef STARFLOOD_RENDER_INTERACTS
+			if(i == 0) {
+				drawLineUV(image, w, h, 1., 1., 0., 1., x_min, y_min, x_max, y_min);
+				drawLineUV(image, w, h, 1., 1., 0., 1., x_min, y_min, x_min, y_max);
+				drawLineUV(image, w, h, 1., 1., 0., 1., x_min, y_max, x_max, y_max);
+				drawLineUV(image, w, h, 1., 1., 0., 1., x_max, y_min, x_max, y_max);
+				drawLineUV(image, w, h, 1., 0., 0., 1., u_i, v_i, tree[cur_node].x, tree[cur_node].y);
+			}
+			#endif
+
+			acc[2*i+0] += ((dx/dist)*mas[i]*dm)/((dx*dx)+(dy*dy)+(real)0.001);
+			acc[2*i+1] += ((dy/dist)*mas[i]*dm)/((dx*dx)+(dy*dy)+(real)0.001);
 
 			//int quad = (p.x > hx ? 1 : 0) + (p.y > hy ? 2 : 0);
 
@@ -332,9 +417,13 @@ int main(int argc, char** argv) {
 	t1 = omp_get_wtime();
 	int w = 960; // Image Width
 	int h = 540; // Image Height
-	const long long int N = 384; // number of bodies in the simulation
+	const long long int N = 16384; // number of bodies in the simulation
 
-	const real dt = 0.0314159265358979323846264338327950288419716939937510582097494459; // Simulation Timestep
+	if(N < 1) {
+		return EXIT_FAILURE;
+	}
+
+	const real dt = TIMESTEP; // Simulation Timestep
 
 	float* image; // Image Buffer, RGBA32F
 	real* sim; // Simulation Buffer
@@ -352,8 +441,6 @@ int main(int argc, char** argv) {
 	size_t acc_size = (N * 2) * sizeof(real); // acceleration buffer size
 	size_t sim_size = mas_size+pos_size+vel_size+acc_size; // simulation total buffer size
 	size_t ids_size = (N * 1) * sizeof(int);
-
-	printf("%zu\n",sim_size);
 
 	std::vector<Node> tree;
 
@@ -387,25 +474,11 @@ int main(int argc, char** argv) {
 		pos = mas+(mas_size/sizeof(real)); // position buffer
 		vel = pos+(pos_size/sizeof(real)); // velocity buffer
 		acc = vel+(vel_size/sizeof(real)); // acceleration buffer
-
-		printf("mas: %p\n",mas);
-		printf("pos: %p\n",pos);
-		printf("vel: %p\n",vel);
-		printf("acc: %p\n",acc);
-	}
-
-	real tmin_x = (real)-1.;
-	real tmin_y = (real)-1.;
-	real tmax_x = (real)1.;
-	real tmax_y = (real)1.;
-
-	{
-
 	}
 
 	// Initialize Simulation
 	{
-		for(int i = 0; i < (N * 1); i++) mas[i] = (real)1;
+		for(int i = 0; i < (N * 1); i++) mas[i] = (real)(((long double)1)/((long double)N));
 		for(int i = 0; i < (N * 2); i++) acc[i] = (real)0;
 		for(int i = 0; i < (N * 2); i++) vel[i] = (real)0;
 
@@ -427,24 +500,24 @@ int main(int argc, char** argv) {
 			//pos[2*i+2] = 0.1*sqrt(-2.0*log(z1))*cos(TAU*z3);
 		}
 
-		for(int i = 0; i < N; i++) {
-			vel[2*i+0] = -.1*pos[2*i+1];
-			vel[2*i+1] =  .1*pos[2*i+0];
-		}
+		//for(int i = 0; i < N; i++) {
+		//	vel[2*i+0] = -.1*pos[2*i+1];
+		//	vel[2*i+1] =  .1*pos[2*i+0];
+		//}
 
 		// calculate initial gravitational accelerations
-		BarnesHut(tree, image, w, h, ids, pos, acc, tmin_x, tmin_y, tmax_x, tmax_y, N);
+		BarnesHut(tree, image, w, h, ids, mas, pos, acc, N);
 	}
 
 	#ifdef STARFLOOD_ENABLE_PROFILING
 	FILE* diagfile = fopen("./log.csv", "w");
 
-	fprintf(diagfile, "\"n\",\"Clear Image (ms)\",\"Render Image (ms)\",\"Write Image (ms)\",\"First Kick (ms)\",\"Drift (ms)\",\"Update Acceleration (ms)\",\"Second Kick (ms)\"\n");
+	fprintf(diagfile, "\"n\",\"Clear Image (ms)\",\"Render Bodies (ms)\",\"Render Quadtree (ms)\",\"Write Image (ms)\",\"First Kick (ms)\",\"Drift (ms)\",\"Update Acceleration (ms)\",\"Second Kick (ms)\"\n");
 
 	fflush(diagfile);
 	#endif
 
-	const int num_steps = 150;
+	const int num_steps = FRAMES;
 
 	for(int step_num = 0; step_num < num_steps; step_num++) {
 		printf("\rRunning simulation, %d/%d (%.2f%) completed...", step_num, num_steps, 100.0*((double)step_num/(double)num_steps));
@@ -465,10 +538,12 @@ int main(int argc, char** argv) {
 				t0 = omp_get_wtime();
 				#endif
 
-				// VERY FAST
-				//#pragma omp simd
-				for(int i = 0; i < (4*w*h); i++) image[i] = (float)0;
-			
+				if((step_num % FRAME_INTERVAL) == 0) {
+					// VERY FAST
+					//#pragma omp simd
+					for(int i = 0; i < (4*w*h); i++) image[i] = (float)0;
+				}
+
 				#ifdef STARFLOOD_ENABLE_PROFILING
 				t1 = omp_get_wtime();
 				fprintf(diagfile, "%.6f,", 1000.0*(t1-t0));
@@ -480,26 +555,28 @@ int main(int argc, char** argv) {
 				#ifdef STARFLOOD_ENABLE_PROFILING
 				t0 = omp_get_wtime();
 				#endif
-			
-				for(int i = 0; i < N; i++) {
-					// vec2 uv = (fragCoord - 0.5 * resolution) / resolution.y
-					real t = 0.04*(real)step_num;
-					//t = 0.0;
-					//real uv[2] = {r[2 * i + 0]*cos(t)+r[2 * i + 2]*sin(t), r[2 * i + 1]};
-					real uv[2] = {pos[2*i+0], pos[2*i+1]};
 
-					uv[0] *= (real)1.0; // 0.05
-					uv[1] *= (real)1.0;
+				if((step_num % FRAME_INTERVAL) == 0){
+					for(int i = 0; i < N; i++) {
+						// vec2 uv = (fragCoord - 0.5 * resolution) / resolution.y
+						real t = 0.04*(real)step_num;
+						//t = 0.0;
+						//real uv[2] = {r[2 * i + 0]*cos(t)+r[2 * i + 2]*sin(t), r[2 * i + 1]};
+						real uv[2] = {pos[2*i+0], pos[2*i+1]};
 
-					int coord[2] = {
-					(int)((real)h * uv[0] + (real)0.5 * (real)w),
-					(int)((real)h * uv[1] + (real)0.5 * (real)h)};
+						uv[0] *= (real)1.0; // 0.05
+						uv[1] *= (real)1.0;
 
-					if((0 <= coord[0]) && (coord[0] < w) && (0 <= coord[1]) && (coord[1] < h)) {
-						image[4*(w*coord[1]+coord[0])+0] += 0.5f;
-						image[4*(w*coord[1]+coord[0])+1] += 0.5f;
-						image[4*(w*coord[1]+coord[0])+2] += 0.5f;
-						image[4*(w*coord[1]+coord[0])+3] += 1.0f;
+						int coord[2] = {
+						(int)((real)h * uv[0] + (real)0.5 * (real)w),
+						(int)((real)h * uv[1] + (real)0.5 * (real)h)};
+
+						if((0 <= coord[0]) && (coord[0] < w) && (0 <= coord[1]) && (coord[1] < h)) {
+							image[4*(w*coord[1]+coord[0])+0] += 0.5f;
+							image[4*(w*coord[1]+coord[0])+1] += 0.5f;
+							image[4*(w*coord[1]+coord[0])+2] += 0.5f;
+							image[4*(w*coord[1]+coord[0])+3] += 1.0f;
+						}
 					}
 				}
 
@@ -509,26 +586,57 @@ int main(int argc, char** argv) {
 				#endif
 			}
 
-			BarnesHut(tree, image, w, h, ids, pos, acc, tmin_x, tmin_y, tmax_x, tmax_y, N); // if you want tree rendered
+			{
+				#ifdef STARFLOOD_ENABLE_PROFILING
+				t0 = omp_get_wtime();
+				#endif
 
-			for(int i = 0; i < (w * h); i++) {
-				image[4*i+0] /= (image[4*i+3] != (float)0) ? image[4*i+3] : (float)1;
-				image[4*i+1] /= (image[4*i+3] != (float)0) ? image[4*i+3] : (float)1;
-				image[4*i+2] /= (image[4*i+3] != (float)0) ? image[4*i+3] : (float)1;
-				image[4*i+3]  = (float)1;
+				if((step_num % FRAME_INTERVAL) == 0) {
+					for(int i = 0; i < tree.size(); i++) {
+						real x_min = tree[i].x_min, y_min = tree[i].y_min, x_max = tree[i].x_max, y_max = tree[i].y_max;
+
+						if((x_max-x_min) < (real)0.1) continue;
+
+						drawLineUV(image, w, h, 0., .01, 0., .1, x_min, y_min, x_max, y_min);
+						drawLineUV(image, w, h, 0., .01, 0., .1, x_min, y_min, x_min, y_max);
+						drawLineUV(image, w, h, 0., .01, 0., .1, x_min, y_max, x_max, y_max);
+						drawLineUV(image, w, h, 0., .01, 0., .1, x_max, y_min, x_max, y_max);
+					}
+				}
+
+				#ifdef STARFLOOD_ENABLE_PROFILING
+				t1 = omp_get_wtime();
+				fprintf(diagfile, "%.6f,", 1000.0*(t1-t0));
+				#endif
+			}
+
+			if((step_num % FRAME_INTERVAL) == 0){
+				#ifdef STARFLOOD_RENDER_INTERACTS
+				BarnesHut(tree, image, w, h, ids, mas, pos, acc, N);
+				#endif
+
+				for(int i = 0; i < (w * h); i++) {
+					image[4*i+0] /= (image[4*i+3] != (float)0) ? image[4*i+3] : (float)1;
+					image[4*i+1] /= (image[4*i+3] != (float)0) ? image[4*i+3] : (float)1;
+					image[4*i+2] /= (image[4*i+3] != (float)0) ? image[4*i+3] : (float)1;
+					image[4*i+3]  = (float)1;
+				}
 			}
 
 			// write the image
 			{
 				char file_name[FILENAME_MAX];
 
-				snprintf(file_name, FILENAME_MAX, "out/step_%04d.hdr", step_num);
+				snprintf(file_name, FILENAME_MAX, "out/step_%04d.hdr", step_num/FRAME_INTERVAL);
+
 				#ifdef STARFLOOD_ENABLE_PROFILING
 				t0 = omp_get_wtime();
 				#endif
 
-				if(stbi_write_hdr(file_name, w, h, 4, image) == 0) {
-					printf("Failure on step %d\n", step_num);
+				if((step_num % FRAME_INTERVAL) == 0){
+					if(stbi_write_hdr(file_name, w, h, 4, image) == 0) {
+						printf("Failure on step %d\n", step_num);
+					}
 				}
 
 				#ifdef STARFLOOD_ENABLE_PROFILING
@@ -570,8 +678,6 @@ int main(int argc, char** argv) {
 			#endif
 		}
 
-		if(step_num == 10) printf("%f\n", acc[30]);
-
 		// update acceleration
 		{
 			for(int i = 0; i < (N * 2); i++) acc[i] = (real)0;
@@ -580,7 +686,7 @@ int main(int argc, char** argv) {
 			t0 = omp_get_wtime();
 			#endif
 
-			BarnesHut(tree, image, w, h, ids, pos, acc, tmin_x, tmin_y, tmax_x, tmax_y, N);
+			BarnesHut(tree, image, w, h, ids, mas, pos, acc, N);
 
 			#ifdef STARFLOOD_ENABLE_PROFILING
 			t1 = omp_get_wtime();
